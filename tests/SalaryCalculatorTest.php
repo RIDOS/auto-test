baf98044715ef94c90f648ad59228e77fcab4035
Azat Imaev <smartsites.dev27@gmail.com>
2024-01-18 18:19:49 +0500

    issue1 / Добавил dataProvider для тестов.

--- tests/SalaryCalculatorTest.php
+++ tests/SalaryCalculatorTest.php
@@ -15,13 +15,48 @@ class SalaryCalculatorTest extends TestCase
     /**
      * Тестирование вычисления заработной платы сотрудников.
      * 
+     * @dataProvider dataProvider
+     * 
+     * @param float $salary Заработная плата.
+     * @param float $expected Ожидаемое значение.
+     * 
      * @retun void
      */
-    public function testCalculate(): void
+    public function testCalculate(float $salary, float $expected): void
     {
         $salaryCalculator = new SalaryCalculator();
-        $result = $salaryCalculator->calculate(13850.90);
+        $result = $salaryCalculator->calculate($salary);
 
-        self::assertEquals(11080.72, $result);
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * Тестовы данные.
+     * 
+     * Пример:
+     *  Заработная плата - 13850.90,
+     *  Ожидаемый результат - 11080.72.
+     * 
+     * @return array Все тестовые параметры.
+     */
+    public static function dataProvider(): array
+    {
+        return [
+            [
+                13850.90, 11080.72
+            ],
+            [
+                10, 8.0
+            ],
+            [
+                20, 16.0
+            ],
+            [
+                10.5, 8.4
+            ],
+            [
+                15.5, 12.4
+            ],
+        ];
     }
 }
